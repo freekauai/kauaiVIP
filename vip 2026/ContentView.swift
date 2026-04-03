b85359da8be6f08621a8017cbfe35e306c5c603b
jw <freekauai@me.com>
2026-04-02 14:22:50 -1000

    Initial Commit

---
+++ vip 2026/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vip 2026
+//
+//  Created by Joey Wray on 4/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vip 2026/vip_2026App.swift
@@ -0,0 +1,17 @@
+//
+//  vip_2026App.swift
+//  vip 2026
+//
+//  Created by Joey Wray on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct vip_2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
